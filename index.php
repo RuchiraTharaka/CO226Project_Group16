bfeb0706e7cf9acd0c15a400cba4164543097d5e
dhananjaya weerasinghe <e18073@eng.pdn.ac.lk>
2022-02-16 15:40:40 +0530

    start to develop the site

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nena Sala</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <!-- Header sections starts -->
+    <div class="header">
+        <div class="logo">
+            <div class="logo-img">
+                <a href="index.php"><img src="images/logo.png" alt=""></a>
+            </div>
+            <div class="logo-text">
+                <a href="index.php">NENA SALA</a>
+            </div>
+        </div>
+
+        <div class="navbar">
+            <a href="./pages/home.php">Home</a>
+            <a href="#Login">Login</a>
+            <a href="#Register">Register</a>
+            <a href="#About">About</a>
+            <a href="#Contact us">Contact us</a>
+        </div>
+    </div>
+    <!-- Headee sections ends -->
+
+
+
+    <script src="script.js"></script>
+</body>
+</html>
