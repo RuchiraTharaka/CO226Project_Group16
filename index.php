e27418800e61e5b9d094eb6ff2843b4768a9d312
dhananjaya weerasinghe <e18073@eng.pdn.ac.lk>
2022-02-17 00:34:19 +0530

    working with home page

--- index.php
+++ index.php
@@ -29,6 +29,38 @@
     </div>
     <!-- Headee sections ends -->
 
+    <section class="banner">
+        <div class="content">
+            <h3>NENA SALA TUTORING PROJECT</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In facilis voluptatibus, assumenda beatae repudiandae sequi? Doloribus molestias odit in ullam sed blanditiis facere praesentium consequuntur officia pariatur placeat possimus dignissimos perferendis architecto ipsum porro minima, itaque omnis facilis nam quis nemo. Itaque corrupti natus modi cum in numquam officia saepe!</p>
+            <a href="#" class="btn">View More</a>
+        </div>
+    </section>
+
+    <section class="container">
+        <h3>upcoming sessions...</h3>
+        <div class="session-one">
+            <h5>session 1</h5>
+            <table>
+                <tr>
+                    <td>date:</td>
+                    <td>12-12-2022</td>
+                </tr>
+                <tr>
+                    <td>time:</td>
+                    <td>12:30:00</td>
+                </tr>
+                <tr>
+                    <td>topic:</td>
+                    <td>introduction to arduino</td>
+                </tr>
+                <tr>
+                    <td>center name:</td>
+                    <td>mawanella central college</td>
+                </tr>
+            </table>
+        </div>
+    </section>
 
 
     <script src="script.js"></script>
